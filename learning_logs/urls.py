0c2e35806dbf9d67b2d30211c62e67743f0847c0
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-04-30 17:40:41 +0300

    Add editing entires

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,8 +7,14 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    # Страница для отображения тем
     path('topics/', views.topics, name='topics'),
+    # Страница для отображения темы
     path('topic/<int:topic_id>/', views.topic, name='topic'),
+    # Страница для создания новой темы 
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Страница для создания записи
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Страница для редактирования записи
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
