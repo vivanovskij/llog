9dfc340b0a0439cf764538de4e0a2ed7fb0eaba5
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-04-30 06:27:38 +0300

    Add page Topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>', views.topic, name='topic'),
 ]
